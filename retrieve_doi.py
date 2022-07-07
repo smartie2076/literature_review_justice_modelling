4bf6f7d398bf9ba0d10de702467023a1bf4926d1
smartie2076 <martha.hoffmann@rl-institut.de>
2022-07-07 19:05:41 +0200

    Fetching metadata based on DOI - Input files is the Google Form output: 2022-07-05-Google-Form-Response-Leon-Version-2022-05-11.xlsx - Script: retrieve_doi.py - Output is an updated input file including columns for Metadata (Author, Year, Title, Journal): 2022-07-05-Google-Form-Response-Leon-Version-2022-05-11-metadata.xlsx

---
+++ retrieve_doi.py
@@ -0,0 +1,40 @@
+import requests
+import pandas as pd
+
+def read_file(filename):
+	print(f"Reading from file {filename}")
+	data = pd.read_excel(filename)
+	print(f"Column titles: {data.columns}")
+	print(data.head())
+	return data
+
+def doi2bib(doi):
+  """
+  Return a bibTeX string of metadata for a given DOI.
+  """
+
+  url = "http://dx.doi.org/" + doi
+
+  headers = {"accept": "application/x-bibtex"}
+  r = requests.get(url, headers = headers)
+
+  return r.text
+
+file = "2022-07-05-Google-Form-Response-Leon-Version-2022-05-11.xlsx"
+
+data = read_file(file)
+import bibtexparser
+
+
+for index, row in data.iterrows():
+    print(index)
+    doi = row["DOI"]
+    bibtex = doi2bib(doi)
+    bibjson = bibtexparser.loads(bibtex).entries
+    if "DOI Not Found" not in bibtex:
+        data.loc[index,"Author"] = bibjson[0]["author"]
+        data.loc[index, "Year"] = bibjson[0]["year"]
+        data.loc[index,"Title"] = bibjson[0]["title"]
+        data.loc[index,"Journal"] = bibjson[0]["journal"]
+
+data.to_excel(f'{file[:-5]}-metadata.xlsx')

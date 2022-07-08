6ebabba5d9642a1503f20d612da77f220c118c8b
smartie2076 <martha.hoffmann@rl-institut.de>
2022-07-08 08:59:23 +0200

    Applied black

--- search_string.py
+++ search_string.py
@@ -1,29 +1,31 @@
 import pandas as pd
 
-file_name = './2022-02-25-Keywords-Literature-Review-Paper-Idea.xlsx'
+file_name = "./2022-02-25-Keywords-Literature-Review-Paper-Idea.xlsx"
+
 
 def join(tab, conjunction):
-	data = pd.read_excel(file_name, sheet_name=tab, header=None)
-	print (data)
-	string = ''
-	for value in data[0].values:
-		if string == '':
-			string = f'"{value}"'
-		else:
-			string += f' {conjunction} "{value}"'
+    data = pd.read_excel(file_name, sheet_name=tab, header=None)
+    print(data)
+    string = ""
+    for value in data[0].values:
+        if string == "":
+            string = f'"{value}"'
+        else:
+            string += f' {conjunction} "{value}"'
 
-	return string
+    return string
 
-tab_and = 'AND'
-tab_or = ['OR (people)', 'OR (justice)']
 
-string_and = join(tab_and, 'AND')
-string_or_people = join(tab_or[0], 'OR')
-string_or_justice = join(tab_or[1], 'OR')
+tab_and = "AND"
+tab_or = ["OR (people)", "OR (justice)"]
 
-search_string = f'{string_and} AND ({string_or_people}) AND ({string_or_justice})'
+string_and = join(tab_and, "AND")
+string_or_people = join(tab_or[0], "OR")
+string_or_justice = join(tab_or[1], "OR")
 
-print (search_string)
+search_string = f"{string_and} AND ({string_or_people}) AND ({string_or_justice})"
+
+print(search_string)
 
 text_file = open("./search_string.txt", "w")
 

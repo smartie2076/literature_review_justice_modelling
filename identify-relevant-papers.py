1bfa757e8d9f1e592d8a6cd439e48476f12ae2cb
smartie2076 <martha.hoffmann@rl-institut.de>
2022-07-08 10:14:20 +0200

    Improve ranking method - Input files now directly merged to one data frame - Operation divided into sensible functions - Relative keyword incidence now based on word count, not letter count - User-defined output-name of the merged and processed file, including sorting based on relative incidence (ie. according to ranking)

--- identify-relevant-papers.py
+++ identify-relevant-papers.py
@@ -1,59 +1,161 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-file_name = '2022-06-10-webofscience-5-search-term-groups-no-transport-0-1000.xls'
-file_name_2 = '2022-06-10-webofscience-5-search-term-groups-no-transport-1001-1126.xls'
+files = [
+    "2022-06-10-webofscience-5-search-term-groups-no-transport-0-1000.xls",
+    "2022-06-10-webofscience-5-search-term-groups-no-transport-1001-1126.xls",
+]
 
-keywords_search_string = ["Energy", "electricit", "heat", "Climate", "decarbon", "sustainab", "renewable", "environment", "green",
-			"carbon", "clean", "transition", "transformation", "development", "pathway", "strateg", "polic", "planning",
-			"model", "indicator", "simulation", "optimi", "tool", "framework", "Consumer", "household", "prosumer",
-			"social", "society", "population", "communit", "justice", "equit", "equality", "fairness"]
-keywords_additional = ["distribution", "representative", "procedual", "burden", "poverty", "fair", "gender", "recognition", "cost", "benefit", "socio-economic", "disadvantage", "minority", "income", "acceptance", "poor"]
+result_file_name = "2022-06-10-webofscience-5-search-term-groups-no-transport-merged"
+
+keywords_search_string = [
+    "Energy",
+    "electricit",
+    "heat",
+    "Climate",
+    "decarbon",
+    "sustainab",
+    "renewable",
+    "environment",
+    "green",
+    "carbon",
+    "clean",
+    "transition",
+    "transformation",
+    "development",
+    "pathway",
+    "strateg",
+    "polic",
+    "planning",
+    "model",
+    "indicator",
+    "simulation",
+    "optimi",
+    "tool",
+    "framework",
+    "Consumer",
+    "household",
+    "prosumer",
+    "social",
+    "society",
+    "population",
+    "communit",
+    "justice",
+    "equit",
+    "equality",
+    "fairness",
+]
+keywords_additional = [
+    "distribution",
+    "representative",
+    "procedual",
+    "burden",
+    "poverty",
+    "fair",
+    "gender",
+    "recognition",
+    "cost",
+    "benefit",
+    "socio-economic",
+    "disadvantage",
+    "minority",
+    "income",
+    "acceptance",
+    "poor",
+]
 keywords = keywords_search_string + keywords_additional
 
-keyword_string = ""
-for keyword in keywords:
-	keyword_string += keyword + "|"
-keyword_string = keyword_string[:-1]
+SEARCH_KEYS = "search-keys"
+ADDITIONAL_KEYS = "additional-keys"
 
+RELATIVE_COUNT = "Relative-Count"
 COUNT = "Count"
-TOTAL_COUNT = "Total Count"
+TOTAL_COUNT = "Total-Count"
 TITLE = "Article Title"
 ABSTRACT = "Abstract"
+HISTOGRAM = "Histogram"
+WORD_COUNT = "Word_Count"
+
+title_and_abstract = TITLE + " AND " + ABSTRACT
+
 
 def read_file(filename):
-	print(f"Reading from file {filename}")
-	data = pd.read_excel(filename)
-	print(f"Column titles: {data.columns}")
-	print(data.head())
-	return data
+    data = pd.DataFrame()
+
+    for file in files:
+        print(f"Reading from file {file}")
+        file_data = pd.read_excel(file)
+        data = pd.concat([data, file_data], ignore_index=True)
+
+    print(f"Column titles: {data.columns}")
+    print(data.head())
+    print(f"Number of entries: {len(data.index)}")
+    data.drop_duplicates([TITLE, ABSTRACT])
+    print(f"Number of entries after removing duplicates: {len(data.index)}")
+
+    return data
+
+
+def get_keyword_string():
+    keyword_string = ""
+    for keyword in keywords:
+        keyword_string += keyword + "|"
+    keyword_string = keyword_string[:-1]
+    print(f"Number of keywords: {len(keywords)}")
+    return keyword_string
+
 
 def counting_keyword_relevance(data, column):
-	for keyword in keywords:
-		data[COUNT + "-" + keyword] = data[column].str.count(keyword)
-	data[TOTAL_COUNT] = data[column].str.count(keyword_string)
-	data[TOTAL_COUNT+"-search-terms"] = sum(data[COUNT+"-"+keyword] for keyword in keywords_search_string)
-	data[TOTAL_COUNT+"-additional-terms"] = sum(data[COUNT+"-"+keyword] for keyword in keywords_additional)
-	return data
+    keyword_string = get_keyword_string()
+    for keyword in keywords:
+        data[COUNT + "-" + keyword] = data[column].str.count(keyword)
+    data[TOTAL_COUNT] = data[column].str.count(keyword_string)
+    data[TOTAL_COUNT + "-" + SEARCH_KEYS] = sum(
+        data[COUNT + "-" + keyword] for keyword in keywords_search_string
+    )
+    data[TOTAL_COUNT + "-" + ADDITIONAL_KEYS] = sum(
+        data[COUNT + "-" + keyword] for keyword in keywords_additional
+    )
+    return data
 
-title_and_abstract = TITLE+" AND " + ABSTRACT
-data = read_file(file_name)
-data.append(read_file(file_name_2))
-print(f"Number of keywords: {len(keywords)}")
-data[title_and_abstract] = data[TITLE] + data[ABSTRACT]
-data["wordcount-"+title_and_abstract] = [len(data[title_and_abstract][i]) for i in data.index]
-data = counting_keyword_relevance(data, title_and_abstract)
-data["relative-indidence-total"] = data[TOTAL_COUNT] / data["wordcount-"+title_and_abstract]
-data[TOTAL_COUNT].plot.hist(bins=20)
-plt.xlabel('Incidence of keywords in title and abstract')
-plt.ylabel('Number of papers')
-len = len("2022-06-10-webofscience-5-search-term-groups-no-transport")
-plt.savefig(f'./{file_name[:len]}-histogram-{TOTAL_COUNT}.png')
-plt.close()
-data["relative-indidence-total"].plot.hist(bins=20)
-plt.xlabel('Incidence of keywords in title and abstract, relative to word count')
-plt.ylabel('Number of papers')
-plt.savefig(f'./{file_name[:len]}-histogram-relative-indidence-total.png')
-plt.close()
 
-data.to_excel(f'{file_name[:len]}-{COUNT}.xlsx')
+def process_counts_to_plots(data):
+    # Word count
+    data[f"{WORD_COUNT}-{title_and_abstract}"] = [
+        len(str(data[title_and_abstract][i]).split()) for i in range(0, len(data.index))
+    ]
+    data[f"{WORD_COUNT}-{title_and_abstract}"].plot.hist(bins=20)
+    plt.xlabel("Number of words in title and abstract")
+    plt.ylabel("Number of papers")
+    plt.savefig(f"./{result_file_name}-{HISTOGRAM}-{WORD_COUNT}.png")
+    plt.close()
+
+    # Absolute keyword incidence
+    data[TOTAL_COUNT].plot.hist(bins=20)
+    plt.xlabel("Count of keywords in title and abstract")
+    plt.ylabel("Number of papers")
+    plt.savefig(f"./{result_file_name}-{HISTOGRAM}-{TOTAL_COUNT}.png")
+    plt.close()
+
+    # Keyword incidence relative to word count
+    data[RELATIVE_COUNT] = data[TOTAL_COUNT] / data[f"{WORD_COUNT}-{title_and_abstract}"]
+    data[RELATIVE_COUNT].plot.hist(bins=20)
+    plt.xlabel("Count of keywords in title and abstract, relative to word count")
+    plt.ylabel("Number of papers")
+    plt.savefig(f"./{result_file_name}-{HISTOGRAM}-{RELATIVE_COUNT}.png")
+    plt.close()
+
+
+def assess_relevance_of_papers():
+    data = read_file(files)
+
+    data[title_and_abstract] = data[TITLE] + data[ABSTRACT]
+
+    data = counting_keyword_relevance(data, title_and_abstract)
+    process_counts_to_plots(data)
+
+    data = data.sort_values(by=[RELATIVE_COUNT], ascending=False, ignore_index=True)
+    data.to_csv(f"{result_file_name}-{COUNT}.csv")
+
+
+assess_relevance_of_papers()

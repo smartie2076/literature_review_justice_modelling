3a8bfd90f6cbf101c2ee9a0348d8fa7166996df8
smartie2076 <martha.hoffmann@rl-institut.de>
2022-07-20 13:00:50 +0200

    Fix inclusion_exclusion_criteria.py - Fix bug of terminating script (np.bool not recognized as bool) - Print info in a neater way - Drop "unnamed" columns - "save and quit" when reaching the end of the script

--- inclusion_exclusion_criteria.py
+++ inclusion_exclusion_criteria.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import os
+import numpy as np
 import sys
 
 intro = "Is the paper"
@@ -8,11 +9,11 @@ inclusion_criteria_title = ["implying a relation with energy issues?"]
 inclusion_criteria_title_and_abstract = [
     "dealing with energy supply?",
     "highlighting the relevance of justice in planning?",
-    "investigating the impact of policies on consumers?"
+    "investigating the impact of policies on consumers?",
     "offer a structurized approach via eg a model or framework?",
-    "clearly stating at least one justice indicator?",
-    "hinting at at least one relevant justice indicator?",
-    "otherwise relevant for the paper?",
+    "clearly stating at least one justice indicator? (including fuzzy logic)",
+    "hinting at at least one relevant justice indicator? (only True if no explicit mention)",
+    "otherwise relevant for the paper? (only True if otherwise not included)",
     "otherwise relevant for my PhD?",
     "otherwise relevant for people I know?"
 ]
@@ -64,42 +65,68 @@ KEYWORD_LIST = [
 ]
 KEYWORD_LIST += [key.capitalize() for key in KEYWORD_LIST]
 
+def assess_titles(data):
+    print(
+        f"Number of assessed titles: {len(data.index) - data[inclusion_criteria_title[0]].isna().sum()}")
+    print(
+        f"Number of potentially relevant titles: {data[inclusion_criteria_title[0]].sum()}")
+    print(
+        f"Number of titles left to assess: {data[inclusion_criteria_title[0]].isna().sum()}\n")
+
+def assess_title_and_abstract(data):
+    if all([key in data.columns for key in inclusion_criteria_title_and_abstract]):
+        print(f"Number of assessed papers (estimate): {len(data.index)-data[inclusion_criteria_title_and_abstract[0]].isna().sum()}")
+        print(
+            f"Number of papers left to assess (estimate): {data[inclusion_criteria_title_and_abstract[0]].isna().sum() - (len(data.index) - data[inclusion_criteria_title[0]].sum())}\n"
+        )
 
 def bold_keys(string):
     for word in KEYWORD_LIST:
         string = string.replace(word, "\033[1m"+word+ '\033[0m')
     return string
 
-def paper_author_year_title_abstract(data, paper_id, title):
+def paper_author_year_title_abstract(data, paper_id, title, displayed_paper_info):
     # Display paper info on screen
-    print(f"\n {data[AUTHORS][paper_id]}")
-    print(f"{data[YEAR][paper_id]}\n")
-    print(f"{bold_keys(data[TITLE][paper_id])}\n")
-    if title is False:
-        print(f"{bold_keys(data[ABSTRACT][paper_id])}\n \n")
-    print(intro)
+    if displayed_paper_info is False:
+
+        print(f"\n {data[AUTHORS][paper_id]}")
+        print(f"{data[YEAR][paper_id]}\n")
+        print(f"{bold_keys(data[TITLE][paper_id])}\n")
+        if title is False:
+            print(f"{bold_keys(data[ABSTRACT][paper_id])}\n \n")
+        print(intro)
+        displayed_paper_info = True
+
+    return displayed_paper_info
 
 def get_vote_on_paper(data, paper_id, inclusion_criteria, title = False):
+    displayed_paper_info = False
     for i in range(0, len(inclusion_criteria)):
         skip = 0
         try:
-            # Check if the old file already included a vote on the inclusion criteria
-            if (
-                data.loc[paper_id, inclusion_criteria[i]] is True
-                or data.loc[paper_id, inclusion_criteria[i]] is False
-            ):
-                vote = data.loc[paper_id, inclusion_criteria[i]]
-                skip = 2
-            # If keywords from group energy services, pass title as relevant
-            elif (title is True) and ("Energ" in data[TITLE][paper_id] or "energ" in data[TITLE][paper_id] or "electr" in data[TITLE][paper_id] or "Electr" in data[TITLE][paper_id] or "heat" in data[TITLE][paper_id] or "Heat" in data[TITLE][paper_id]):
-                vote = True
-                skip = 2
+            if isinstance(data.loc[paper_id, inclusion_criteria[i]], np.bool_) is True or isinstance(data.loc[paper_id, inclusion_criteria[i]], bool) is True:
+                # Entries are read as np.bool_, and those do not pass the test here
+                value = bool(data.loc[paper_id, inclusion_criteria[i]])
+                # Check if the old file already included a vote on the inclusion criteria
+                if (
+                    value is True
+                    or value is False
+                ):
+                    vote = data.loc[paper_id, inclusion_criteria[i]]
+                    skip = 2
+                # If keywords from group energy services, pass title as relevant
+                elif (title is True) and ("Energ" in data[TITLE][paper_id] or "energ" in data[TITLE][paper_id] or "electr" in data[TITLE][paper_id] or "Electr" in data[TITLE][paper_id] or "heat" in data[TITLE][paper_id] or "Heat" in data[TITLE][paper_id]):
+                    vote = True
+                    skip = 2
+                else:
+                    # No value for inclusion criteria stored - print information to receive a vote
+                    displayed_paper_info = paper_author_year_title_abstract(data, paper_id, title, displayed_paper_info)
             else:
                 # No value for inclusion criteria stored - print information to receive a vote
-                paper_author_year_title_abstract(data, paper_id, title)
+                displayed_paper_info = paper_author_year_title_abstract(data, paper_id, title, displayed_paper_info)
         except:
             # May happen if the column for the inclusion criteria does not exist jet
-            paper_author_year_title_abstract(data, paper_id, title)
+            displayed_paper_info = paper_author_year_title_abstract(data, paper_id, title, displayed_paper_info)
 
         # Get a vote on the inclusion criteria for the paper.
         # Valid inputs: Affirmative, Negative, Pass, Exit (premature script exit)
@@ -133,22 +160,15 @@ def save_and_quit(data):
     # In case of manual termination or end of data
     data.to_csv(output_file)
     print(f"Saved outputs to {output_file}.")
-    print(
-        f"Number of assessed titles: {len(data.index) - data[inclusion_criteria_title[0]].isna().sum()}")
-    print(
-        f"Number of potentially relevant titles: {data[inclusion_criteria_title[0]].sum()}")
-
+    assess_titles(data)
     # Only works if all columns have already been created, ie. if all titles have been pre-selected already.
+    assess_title_and_abstract(data)
     if all([key in data.columns for key in inclusion_criteria_title_and_abstract]):
-        print(f"Number of assessed papers (estimate): {len(data.index)-data[inclusion_criteria_title_and_abstract[0]].isna().sum()}")
-        print(
-            f"Number of papers left to assess (estimate): {data[inclusion_criteria_title_and_abstract[0]].isna().sum()}"
-        )
         data["Include"]=[sum([data.loc[id, inclusion_criteria_title_and_abstract[i]] for i in range(0, 6)]) == 6 for id in data.index]
         print(f"Papers are relevant, if they fullfill the following inclusion criteria: "
               f"\n Does the title {inclusion_criteria_title[0]} And does the paper... {inclusion_criteria_title_and_abstract[0:5]}.")
         print(f"Intermediate number of relevant papers: {sum(data['Include'])}")
-        print(f"Intermediate number of otherwise relevant papers: f{sum(data[inclusion_criteria_title_and_abstract[6]])}")
+        print(f"Intermediate number of otherwise relevant papers: {data[inclusion_criteria_title_and_abstract[6]].sum()}")
     sys.exit()
 
 
@@ -157,13 +177,15 @@ def evaluate_title_and_abstract():
     print(f"Original number of literature to assess: {len(data.index)}")
     data_with_votes = pd.read_csv(output_file)
     data = data.merge(data_with_votes, how="left")
+    data.drop(data.columns[data.columns.str.contains('unnamed',case = False)],axis = 1, inplace = True)
+
     print(
         f"Number of literature to assess after merge with old votes: {len(data.index)}"
     )
+
     try:
-        print(
-            f"Number of papers left to assess (approximately): {data[inclusion_criteria_title_and_abstract[0]].isna().sum()}"
-        )
+        assess_titles(data)
+        assess_title_and_abstract(data)
     except:
         pass
 
@@ -172,9 +194,19 @@ def evaluate_title_and_abstract():
         get_vote_on_paper(data, paper_id=i, inclusion_criteria=inclusion_criteria_title, title = True)
 
     for i in data.index:
+        # Boolean values stored as np.bool_
+        try:
+            value = bool(data.loc[i, inclusion_criteria_title[0]])
+        except:
+            value = None
+            print(f"The title inclusion criteria of paper {data.loc[i,TITLE]} is None, as DataFrame returns {data.loc[i, inclusion_criteria_title[0]]}.")
+
         # Only assess entry if title fullfills inclusion criteria for the title
-        if data.loc[id, inclusion_criteria_title[0]] is True or data.loc[id, inclusion_criteria_title[0]] is None:
+        if value is True or value is None:
             # Assess the inclusion criteria for the abstract and title together
             get_vote_on_paper(data, paper_id=i, inclusion_criteria=inclusion_criteria_title_and_abstract)
+
+    # Save and quit when program end is reached:
+    save_and_quit(data)
 
 evaluate_title_and_abstract()

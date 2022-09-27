5b10de23435f262b0cc7c53a0728071a7e26f178
smartie2076 <martha.hoffmann@rl-institut.de>
2022-09-27 19:54:56 +0200

    Second screening

---
+++ screening_second.py
@@ -0,0 +1,51 @@
+import inclusion_exclusion_criteria
+
+screened_by = "Martha"
+# screened_by = "Jonathan"
+
+# Keeping this criteria, as otherwise too much code to change
+inclusion_criteria_title = ["implying a relation with energy issues?"]
+
+inclusion_criteria_title_and_abstract = [
+    "Does the paper define justice indicators? (0=nope, 1=probably/on the side, 2=yes, 3=exeptional)",
+    "Does the paper implement justice indicators? (0=nope, 1=probably/on the side, 2=yes, 3=exeptional)",
+    "Does the paper assess injustices between different defined groups? (0=nope, 1=probably/on the side, 2=yes, 3=exeptional)"
+    "Is the paper describing justice in (0) a numerical approach, (1) a theoretical framework or (2) with other means?",
+    "Which topic does the paper mainly address? (0) carbon emissions, (1) carbon pricing or tax, (2) SDG, (3) global perspective, (4) electrification?",
+    "Paper must be included! (Veto)",
+    "Paper is included based on the veto above, but is only relevant for the backgroud  section.",
+]
+
+dict_options_inclusion_criteria_title_and_abstract = {
+    inclusion_criteria_title_and_abstract[0]: [0, 1, 2, 3],
+    inclusion_criteria_title_and_abstract[1]: [0, 1, 2, 3],
+    inclusion_criteria_title_and_abstract[2]: [0, 1, 2],
+    inclusion_criteria_title_and_abstract[3]: [0, 1, 2, 3],
+    inclusion_criteria_title_and_abstract[4]: [True, False],
+    inclusion_criteria_title_and_abstract[5]: [True, False],
+}
+
+exclusion_criteria = [""]
+
+file = "2022-07-19-Final-Keywords-Publications-merged-Count-votes-only-included.csv"
+output_file = file[:-4] + "-2nd-screening"
+if screened_by == "Martha":
+    output_file += "-m"
+elif screened_by == "Jonathan":
+    output_file += "-j"
+output_file += ".csv"
+
+number_of_inclusion_criteria = 3
+target_value_of_inclusion_criteria = 6
+list_index_positive_vote = 4
+
+inclusion_exclusion_criteria.evaluate_title_and_abstract(
+    file=file,
+    output_file=output_file,
+    inclusion_criteria_title=inclusion_criteria_title,
+    inclusion_criteria_title_and_abstract=inclusion_criteria_title_and_abstract,
+    dict_options_inclusion_criteria_title_and_abstract=dict_options_inclusion_criteria_title_and_abstract,
+    target_value_of_inclusion_criteria=target_value_of_inclusion_criteria,
+    number_of_inclusion_criteria=number_of_inclusion_criteria,
+    list_index_positive_vote=list_index_positive_vote,
+)

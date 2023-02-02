9f58837079143f347faef0b13180271fd2f32605
smartie2076 <martha.hoffmann@rl-institut.de>
2023-02-02 15:06:38 -0800

    File Tree Cleaning: 3rd Screening by Paper Screening

---
+++ 09-Merging-updated-votes-2023-01-24.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+data_revote = pd.read_csv("2023-01-3rd-Screening-Paper-Screening/2023-01-24-Relevant_Articles_Revote.csv", sep=";")
+data_revote.reset_index(drop=True)
+
+data = pd.read_csv(
+    "2022-10-2nd-Screening-Jonathan-Martha-Score-Results/2022-09-30-2nd-screening-only-included-joined.csv")
+data.drop(
+    data.columns[data.columns.str.contains("unnamed", case=False)],
+    axis=1,
+    inplace=True,
+)
+data.reset_index(drop=True)
+
+columns_data_revote = list(data_revote.columns)
+columns_data_revote.remove("Assignee")
+columns_data_revote.remove("DOI")
+
+data_new = data.drop(columns_data_revote, axis=1)
+data_new = data_new.merge(data_revote, how="left", on="DOI")
+data_new.to_csv("2022-09-30-2nd-screening-only-included-joined.csv")
